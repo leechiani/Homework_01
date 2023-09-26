4d0fb7dae5e6650636f261fd07adc983965ad987
Sonya Lee <t1i5n2a8t@gmail.com>
2023-09-26 11:50:53 +0800

    PenTest and related class modified

--- src/hw5/InkBrush.java
+++ src/hw5/InkBrush.java
@@ -2,14 +2,9 @@ package hw5;
 
 public class InkBrush extends Pen {
 
-    private int actualPrice;
-
     public int getPrice() {
-        return actualPrice;
-    }
-
-    public void setPrice(int price) {
-        actualPrice = (int) (price * 0.9);
+        double price = super.getPrice();
+        return (int) (price * 0.9);
     }
 
     public void write() {
--- src/hw5/Pencil.java
+++ src/hw5/Pencil.java
@@ -2,16 +2,11 @@ package hw5;
 
 public class Pencil extends Pen {
 
-    private int actualPrice;
-
     public int getPrice() {
-        return actualPrice;
+        double price = super.getPrice();
+        return (int) (price * 0.8);
     }
-
-    public void setPrice(int price) {
-        actualPrice = (int) (price * 0.8);
-    }
-
+    
     public void write() {
         System.out.println("削鉛筆再寫");
     }
